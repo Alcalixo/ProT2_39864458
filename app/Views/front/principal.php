d3ec51bdd1f284af4e744b09e5232fefb3b45a85
Alcalixo <rodrix_gonzalo@outlook.com>
2024-06-19 20:56:33 -0300

    arreglado instalación de bootstrap, importación del proyecto TP1 como quedó, con todas las imágenes y corregido los src de cada una

---
+++ app/Views/front/principal.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Principal</title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <header>
+        <img id="banner" src="assets/img/banner.jpg" alt="logo">
+        <h1>RPG MARKER STUDIO</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="principal.html">INICIO</a></li>
+            <li><a href="https://www.google.com.ar/" target="_blank">GOOGLE</a></li>
+            <li><a href="https://www.instagram.com/" target="_blank">INSTAGRAM</a></li>
+            <li><a href="https://talentosdigitales.ar/" target="_blank">TALENTOS DIGITALES</a></li>
+        </ul>
+    </nav>
+    <section>
+        <div class="ejercicio4-3">
+            <p id="ejercicio1_2">
+                Somos un equipo de desarrolladores independientes que se dedica a la
+                elaboración de videojuegos, y prestar apoyo a compañias y otros
+                desarrolladores, así como prestar servicio se soporte para corrección
+                de errores post lanzamiento.
+            </p>
+            <p>
+                Nuestra dedicación y pasión por los videojuegos nos permite trabajar
+                continuamente en desarrollar ideas nuevas y pulir otras existentes
+                para intentar darle a los gamers (como nosotros) la mejor experiencia
+                de juego posible.
+            </p>
+        </div>
+
+        <img src="assets/img/ejemplo_1.jpg" alt="imagen1">
+    </section>
+    <footer class="footer-content">
+        <div>
+            <a href="https://www.facebook.com/" target="_blank"><img src="assets/img/facebook-logo.png" alt="facebook" id="facebook" /></a>
+            <a href="https://www.instagram.com/" target="_blank"><img src="assets/img/instagram-logo.png" alt="instagram" id="instagram" /></a>
+            <a href="https://github.com/" target="_blank"><img src="assets/img/github-logo.png" alt="gitHub" id="gitHub" /></a>
+        </div>
+        <br />
+        &#169 Alcalixo 2024 | Todos los Derechos Reservados
+    </footer>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
