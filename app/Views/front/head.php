2d9698d3fbce13cf7a8384aced2268937a8b3f3d
Alcalixo <rodrix_gonzalo@outlook.com>
2024-07-02 18:33:12 -0300

    solucionado carrousel, corrección de enlase sobre 1)imagen del logo, 2)dirección a donde remite el logo, 3)agregado un margen entre tarjetas del staff, 2)agregado margen al carrousel

--- app/Views/front/head.php
+++ app/Views/front/head.php
@@ -18,8 +18,8 @@
         <nav class="navbar navbar-expand-lg">
             <div class="container-fluid">
     <!--Logo de la Empresa-->
-                <a class="navbar-brand" href="<?php echo base_url('principal')?>">
-                    <img src="<?php echo base_url('assets/img/logo.jpg')?>" alt="Logo" width="75" class="d-inline-block align-text-top">
+                <a class="navbar-brand" href="principal">
+                    <img src="assets/img/logo.jpg" alt="Logo" width="75" class="d-inline-block align-text-top">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
--- app/Views/front/principal.php
+++ app/Views/front/principal.php
@@ -16,9 +16,40 @@
       <p>Algunos ejemplos de nuestro trabajo:</p>
     </div>
   </div>
-  <div class="row-10 text-center d-flex justify-content-center">
+  <div class="row-10 text-center justify-content-center">
     <section>
       <!--Carrousel-->
+      <div id="carouselExampleIndicators" class="carousel slide">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="assets/img/chrono_trigger3.jpg" class="rounded d-block w-100" alt="chrono_trigger">
+          </div>
+          <div class="carousel-item">
+            <img src="assets/img/shinsetsu_mahou_shoujo3.jpg" class="rounded d-block w-100" alt="shinsetsu_mahou_shoujo">
+          </div>
+          <div class="carousel-item">
+            <img src="assets/img/dark_deity3.jpg" class="rounded d-block w-100" alt="dark_deity">
+          </div>
+          <div class="carousel-item">
+            <img src="assets/img/desarrollo3.jpg" class="rounded d-block w-100" alt="desarrollo">
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+      <!--
       <div id="carouselExampleIndicators" class="carousel slide">
         <div class="carousel-indicators">
           <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -49,6 +80,7 @@
           <span class="visually-hidden">Next</span>
         </button>
       </div>
+      -->
     </section>
   </div>
 </div>
